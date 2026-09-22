5eea9ca21f3594790864ee92c6ce64ebea28f18b
zaursejfulaev <zaurseifulaev@yandex.ru>
2026-09-22 21:03:24 +0300

    Исправление замечаний по результатам ревью - исправление ошибок стиля - исправление ошибок стиля. Попытка 2

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import java.time.LocalDateTime;
-
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
@@ -9,17 +7,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookItemRequestDto {
-	private long itemId;
+    private long itemId;
 
     @NotNull
-	@FutureOrPresent
-	private LocalDateTime start;
+    @FutureOrPresent
+    private LocalDateTime start;
 
     @NotNull
-	@Future
-	private LocalDateTime end;
+    @Future
+    private LocalDateTime end;
 }
